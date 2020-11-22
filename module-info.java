22c4059ca25526e8d4ffa531a719d5fd8c66d434
Deviant28 <61645011+Deviant28@users.noreply.github.com>
2020-11-22 10:37:31 +0200

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,58 @@
+public class Web_transfer {
+		public static void main(string[]args) {
+		private Account fromAccount;
+		private Account toAccount;
+		private double amount;
+		private String valueDate;
+		private WireTransferStatus status;
+		private Currency currency;
+
+		public Account getFromAccount() {
+			return fromAccount;
+		}
+
+		public void setFromAccount(Account fromAccount) {
+			this.fromAccount = fromAccount;
+		}
+
+		public Account getToAccount() {
+			return toAccount;
+		}
+
+		public void setToAccount(Account toAccount) {
+			this.toAccount = toAccount;
+		}
+
+		public double getAmount() {
+			return amount;
+		}
+
+		public void setAmount(double amount) {
+			this.amount = amount;
+		}
+
+		public String getValueDate() {
+			return valueDate;
+		}
+
+		public void setValueDate(String valueDate) {
+			this.valueDate = valueDate;
+		}
+
+		public WireTransferStatus getStatus() {
+			return status;
+		}
+
+		public void setStatus(WireTransferStatus status) {
+			this.status = status;
+		}
+
+		public Currency getCurrency() {
+			return currency;
+		}
+
+		public void setCurrency(Currency currency) {
+			this.currency = currency;
+		}
+
+	}
